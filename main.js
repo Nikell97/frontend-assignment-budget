2e7e1c84ffabc255270816bef3df9bbd69b2478b
Nikell97 <nikellmattias@gmail.com>
2023-03-09 13:44:53 +0100

    push not working

--- main.js
+++ main.js
@@ -2,11 +2,21 @@ Vue.createApp({
     data() {
         return {
             // Enter data here
-            
+            activeTab: "month",
         }
     },
     methods: {
         // Enter all methods/functions here
-
+        addExpense(){
+            let expenseObject = {
+                category: this.categoryText,
+                //expense: this.expenseText,
+                //date: this.dateText
+            };
+            this.expenseList.push(expenseObject);
+            this.categoryText = "";
+            this.expenseText = "";
+            this.dateText = "";
+        },
     }
 }).mount('#app');
