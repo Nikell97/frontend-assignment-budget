7ec6f92e26745f8e981687ae09d059cadacf9a4b
Nikell97 <nikellmattias@gmail.com>
2023-03-09 11:22:36 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+Vue.createApp({
+    data() {
+        return {
+            // Enter data here
+            
+        }
+    },
+    methods: {
+        // Enter all methods/functions here
+
+    }
+}).mount('#app');
