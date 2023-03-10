72bde2a18f86d3df90c7635c44722618cac45bb8
Victor Sjöberg <vsjberg13@gmail.com>
2023-03-10 14:30:18 +0100

    Gott the list to work.
    But why in hell does it need to be expense.category//expense//date??? what is "expense"?

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ Vue.createApp({
         return {
             // Enter data here
             activeTab: "month",
+            categoryText: "",
+            expenseText: "",
+            dateText: "",
+            expenseList: [] 
         }
     },
     methods: {
@@ -10,13 +14,25 @@ Vue.createApp({
         addExpense(){
             let expenseObject = {
                 category: this.categoryText,
-                //expense: this.expenseText,
-                //date: this.dateText
+                expense: this.expenseText,
+                date: this.dateText
             };
             this.expenseList.push(expenseObject);
             this.categoryText = "";
             this.expenseText = "";
             this.dateText = "";
         },
+        deleteExpense(index) {
+            this.expenseList.splice(index, 1);
+        },
+        sortByCategory() {
+            // Add sorting logic here
+        },
+        sortByExpense() {
+            // Add sorting logic here
+        },
+        sortByMonth() {
+            // Add sorting logic here
+        }
     }
 }).mount('#app');
