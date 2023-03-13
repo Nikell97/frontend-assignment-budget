aad1249f9462d5e4e94fa1ac7240bdd86f258195
Nikell97 <nikellmattias@gmail.com>
2023-03-13 16:29:02 +0100

    Vue now adding input information into  <li>

--- main.js
+++ main.js
@@ -3,19 +3,27 @@ Vue.createApp({
         return {
             // Enter data here
             activeTab: "month",
+            expenseList: [],
+            categoryText: "",
+            amountText: "",
+            dateText: "",
         }
     },
     methods: {
         // Enter all methods/functions here
         addExpense(){
+            if (!this.dateText) {
+                return; // exit early if dateText is undefined
+            }
+            
             let expenseObject = {
                 category: this.categoryText,
-                //expense: this.expenseText,
-                //date: this.dateText
+                amount: this.amountText,
+                date: this.dateText
             };
             this.expenseList.push(expenseObject);
             this.categoryText = "";
-            this.expenseText = "";
+            this.amountText = "";
             this.dateText = "";
         },
     }
