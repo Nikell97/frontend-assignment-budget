963bbc64060e7fe5855f0bc23ceeac6f11f86e88
Victor Sjöberg <vsjberg13@gmail.com>
2023-03-16 11:55:55 +0100

    The diagram now works!

--- main.js
+++ main.js
@@ -3,17 +3,32 @@ Vue.createApp({
         return {
             // Enter data here
             activeTab: "category",
+
             expenseList: [],
             categoryText: "",
             amountText: "",
             dateText: "",
-            showEmptyFieldsPopup: false
-            
+            showEmptyFieldsPopup: false,
+
+            monthList: [
+                { name: "Jan", amount: 0 },
+                { name: "Feb", amount: 0 },
+                { name: "Mar", amount: 0 },
+                { name: "Apr", amount: 0 },
+                { name: "May", amount: 0 },
+                { name: "Jun", amount: 0 },
+                { name: "Jul", amount: 0 },
+                { name: "Aug", amount: 0 },
+                { name: "Sep", amount: 0 },
+                { name: "Oct", amount: 0 },
+                { name: "Nov", amount: 0 },
+                { name: "Dec", amount: 0 }
+            ],
         }
     },
     methods: {
         // Enter all methods/functions here
-        addExpense(){
+        addExpense() {
             if (this.categoryText == "" || this.amountText == "" || this.dateText == "") {
                 this.showEmptyFieldsPopup = true;
                 return; // There has to be something in all input fields for an item to be created.
@@ -31,15 +46,16 @@ Vue.createApp({
             this.dateText = "";
 
             this.sortHandler();
+            this.monthDiagram();
         },
         deleteExpense(index) {
             this.expenseList.splice(index, 1);
         },
         sortHandler() {
-            if (this.activeTab == "category"){
+            if (this.activeTab == "category") {
                 this.sortByCategory();
             }
-            else if (this.activeTab == "amount"){
+            else if (this.activeTab == "amount") {
                 this.sortByAmount();
             }
             else {
@@ -66,105 +82,59 @@ Vue.createApp({
         },
         calcTotalExpense() {
             let total = 0;
-            for (let expense of this.expenseList){
+            for (let expense of this.expenseList) {
                 let parsedAmount = parseInt(expense.amount);
                 total += parsedAmount;
             }
             return total;
         },
-        calcTotalExpenseByMonth() {
-            // creates an object/"dictionary" of all the months with a connected numerical value that starts at 0
-            const months = {
-                'Jan' : 0,
-                'Feb' : 0,
-                'Mar' : 0,
-                'Apr' : 0,
-                'May' : 0,
-                'Jun' : 0,
-                'Jul' : 0,
-                'Aug' : 0,
-                'Sep' : 0,
-                'Oct' : 0,
-                'Nov' : 0,
-                'Dec' : 0,
+        monthDiagram() {
+            let last = 0; // This exsist so that we can get the last object in the list.
+            for (let i = 1; i < this.expenseList.length; i++) {
+                last += 1;
+            }
+            
+            let expenseDate = this.expenseList[last].date.split('-'); // splits date string at - and puts the values in an array
+            let expenseMonth = expenseDate[1]; // fetches value of second index, which is the month, based on the required date input format
+            let parsedAmount = parseFloat(this.expenseList[last].amount); // have to parse expense.amount otherwise later use of += will concat instead of add
+
+            if (expenseMonth === "01") {
+                this.monthList[0].amount += parsedAmount;
+            }
+            else if (expenseMonth === "02") {
+                this.monthList[1].amount += parsedAmount;
+            }
+            else if (expenseMonth === "03") {
+                this.monthList[2].amount += parsedAmount;
+            }
+            else if (expenseMonth === "04") {
+                this.monthList[3].amount += parsedAmount;
+            }
+            else if (expenseMonth === "05") {
+                this.monthList[4].amount += parsedAmount;
+            }
+            else if (expenseMonth === "06") {
+                this.monthList[5].amount += parsedAmount;
+            }
+            else if (expenseMonth === "07") {
+                this.monthList[6].amount += parsedAmount;
+            }
+            else if (expenseMonth === "08") {
+                this.monthList[7].amount += parsedAmount;
+            }
+            else if (expenseMonth === "09") {
+                this.monthList[8].amount += parsedAmount;
+            }
+            else if (expenseMonth === "10") {
+                this.monthList[9].amount += parsedAmount;
+            }
+            else if (expenseMonth === "11") {
+                this.monthList[10].amount += parsedAmount;
+            }
+            else if (expenseMonth === "12") {
+                this.monthList[11].amount += parsedAmount;
             }
 
-            for (let expense of this.expenseList){
-                let expenseDate = expense.date.split('-'); // splits date string at - and puts the values in an array
-                let expenseMonth = expenseDate[1]; // fetches value of second index, which is the month, based on the required date input format
-                let parsedAmount = parseInt(expense.amount); // have to parse expense.amount otherwise later use of += will concat instead of add
-
-                if (expenseMonth === "01"){
-                    months.Jan += parsedAmount;
-                }
-                else if (expenseMonth === "02"){
-                    months.Feb += parsedAmount;
-                }
-                else if (expenseMonth === "03"){
-                    months.Mar += parsedAmount;
-                }
-                else if (expenseMonth === "04"){
-                    months.Apr += parsedAmount;
-                }
-                else if (expenseMonth === "05"){
-                    months.May += parsedAmount;
-                }
-                else if (expenseMonth === "06"){
-                    months.Jun += parsedAmount;
-                }
-                else if (expenseMonth === "07"){
-                    months.Jul += parsedAmount;
-                }
-                else if (expenseMonth === "08"){
-                    months.Aug += parsedAmount;
-                }
-                else if (expenseMonth === "09"){
-                    months.Sep += parsedAmount;
-                }
-                else if (expenseMonth === "10"){
-                    months.Oct += parsedAmount;
-                }
-                else if (expenseMonth === "11"){
-                    months.Nov += parsedAmount;
-                }
-                else if (expenseMonth === "12"){
-                    months.Dec += parsedAmount;
-                }
-            }
-        },
+        }
     }
 }).mount('#app');
-
-/*
-function drawGraph(){
-    const w = 200;
-    const h = 200;
-    const svg = createSvg(w, h);
-
-    const background = createSvgRect(0, 0, w, h);
-    background.style.fill = black;
-    svg.append(background);
-
-    const vertical = createSvgRect(w * (5 / 16), 0, w * (2/ 16), h)
-}
-
-function createSvg(width, height) {
-    const svg = createSvgElement('svg');
-    svg.setAttribute('width', width);
-    svg.setAttribute('height', height);
-    document.body.append(svg);
-    return svg;
-}
-
-function createSvgElement(tagName) {
-    return document.createElementNS('http://www.w3.org/2000/svg', tagName);
-}
-
-function createSvgRect(x, y, width, height) {
-    const rect = createSvgElement('rect');
-    rect.setAttribute('width', width);
-    rect.setAttribute('height', height);
-    rect.setAttribute('x', x);
-    rect.setAttribute('y', y);
-    return rect;
-}*/
