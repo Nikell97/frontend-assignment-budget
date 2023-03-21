fb8219e6e3b751c2368c95923c5adb3cb71e5adf
Nikell97 <nikellmattias@gmail.com>
2023-03-21 13:00:31 +0100

    two tests and spacebar

--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -17,17 +17,63 @@ test('write one new expense', async ({ page }) => {
   const expenseAmount = page.getByPlaceholder('Enter amount here');
   const expenseDate = page.getByPlaceholder('Enter date here (y-m-d)');
 
-  // Create one todo item.
+  // Create one expense item.
   await expenseCategory.fill(newBudgetList[0].category);
-  await expenseAmount.fill(newBudgetList[0].amount);
+  await expenseAmount.fill(newBudgetList[0].amount.toString());
   await expenseDate.fill(newBudgetList[0].date)
-  await page.get('button', {name: /submit/i }).click();
+  await page.getByRole('button', {name: /Submit/i }).click();
 
-  let noteList = await page.$('.note-list');
+  let expenseList = await page.$('.expenseList');
 
-  let noteListItem = await noteList.$('li');
+  let expenseListItem = await expenseList.$('>:nth-child(1)');
 
-  let noteListItemText = await noteListItem.$('>:nth-child(2)');
+  let expenseListItemCategory = await expenseListItem.$('>:nth-child(1)');
+  let expenseListItemAmount = await expenseListItem.$('>:nth-child(2)');
+  let expenseListItemDate = await expenseListItem.$('>:nth-child(3)');
 
-  await expect(noteListItemText.textContent()).resolves.toMatch('a');
+  await expect(expenseListItemCategory.textContent()).resolves.toMatch('Cookies');
+  await expect(expenseListItemAmount.textContent()).resolves.toMatch('2000kr');
+  await expect(expenseListItemDate.textContent()).resolves.toMatch('2022-02-15'); 
+});
+
+test('write two new expenses', async ({ page }) => {
+  // create a new expense locator
+  const expenseCategory = page.getByPlaceholder('Enter category here');
+  const expenseAmount = page.getByPlaceholder('Enter amount here');
+  const expenseDate = page.getByPlaceholder('Enter date here (y-m-d)');
+
+  // Create one expense item.
+  await expenseCategory.fill(newBudgetList[0].category);
+  await expenseAmount.fill(newBudgetList[0].amount.toString());
+  await expenseDate.fill(newBudgetList[0].date)
+  await page.getByRole('button', {name: /Submit/i }).click();
+
+  let expenseList = await page.$('.expenseList');
+
+  let expenseListItem = await expenseList.$('>:nth-child(1)');
+
+  let expenseListItemCategory = await expenseListItem.$('>:nth-child(1)');
+  let expenseListItemAmount = await expenseListItem.$('>:nth-child(2)');
+  let expenseListItemDate = await expenseListItem.$('>:nth-child(3)');
+
+  await expect(expenseListItemCategory.textContent()).resolves.toMatch('Cookies');
+  await expect(expenseListItemAmount.textContent()).resolves.toMatch('2000kr');
+  await expect(expenseListItemDate.textContent()).resolves.toMatch('2022-02-15'); 
+
+  await expenseCategory.fill(newBudgetList[1].category);
+  await expenseAmount.fill(newBudgetList[1].amount.toString());
+  await expenseDate.fill(newBudgetList[1].date)
+  await page.getByRole('button', {name: /Submit/i }).click();
+
+  expenseList = await page.$('.expenseList');
+
+  expenseListItem = await expenseList.$('>:nth-child(2)');
+
+  expenseListItemCategory = await expenseListItem.$('>:nth-child(1)');
+  expenseListItemAmount = await expenseListItem.$('>:nth-child(2)');
+  expenseListItemDate = await expenseListItem.$('>:nth-child(3)');
+
+  await expect(expenseListItemCategory.textContent()).resolves.toMatch('Jar Jar');
+  await expect(expenseListItemAmount.textContent()).resolves.toMatch('5000kr');
+  await expect(expenseListItemDate.textContent()).resolves.toMatch('2021-06-20'); 
 });
